6caf707d9db793e6b623d70eb0f997318da3f9d5
jiro4989 <jiroron666@gmail.com>
2019-03-30 23:22:35 +0900

    :sparkles: とりあえず関数を作った

---
+++ algin.go
@@ -0,0 +1,22 @@
+package main
+
+// AlignLeft は文字列を左寄せする。
+// 左寄せは見た目上の文字列の長さで揃える。
+func AlignLeft(lines []string) []string {
+	// TODO
+	return nil
+}
+
+// AlignCenter は文字列を左寄せする。
+// 左寄せは見た目上の文字列の長さで揃える。
+func AlignCenter(lines []string) []string {
+	// TODO
+	return nil
+}
+
+// AlignRight は文字列を左寄せする。
+// 左寄せは見た目上の文字列の長さで揃える。
+func AlignRight(lines []string) []string {
+	// TODO
+	return nil
+}
