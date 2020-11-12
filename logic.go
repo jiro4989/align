019cc5131305c307fbeaa4284513c77cc4689fe4
jiro4989 <jiroron666@gmail.com>
2020-11-12 21:43:09 +0900

    vertical 共通化

--- logic.go
+++ logic.go
@@ -58,3 +58,53 @@ func LogicHorizontalAlign(param LogicHorizontalAlignParam) {
 		}
 	}
 }
+
+type LogicVerticalAlignParam struct {
+	args      []string
+	length    int
+	writeFile bool
+	lineFeed  string
+	f         func([]string, int) []string
+}
+
+func LogicVerticalAlign(param LogicVerticalAlignParam) {
+	var (
+		args      = param.args
+		n         = param.length
+		lf        = param.lineFeed
+		writeFile = param.writeFile
+		f         = param.f
+	)
+
+	// 引数なしの場合は標準入力を処理
+	if len(args) < 1 {
+		args = readStdin()
+		padded := f(args, n)
+		for _, v := range padded {
+			fmt.Println(v)
+		}
+		return
+	}
+
+	for _, fn := range args {
+		b, err := ioutil.ReadFile(fn)
+		if err != nil {
+			panic(err)
+		}
+		s := string(b)
+		lines := strings.Split(s, lf)
+		padded := f(lines, n)
+
+		// ファイル上書き指定があれば上書き
+		if writeFile {
+			b := []byte(strings.Join(padded, lf))
+			if err := ioutil.WriteFile(fn, b, os.ModePerm); err != nil {
+				panic(err)
+			}
+			continue
+		}
+		for _, v := range padded {
+			fmt.Println(v)
+		}
+	}
+}
