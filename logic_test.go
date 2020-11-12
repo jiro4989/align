ccd579f0e2dde1c85b38f0bdbe2f9b31d1a722d7
jiro4989 <jiroron666@gmail.com>
2020-11-12 22:00:57 +0900

    テストコード追加

--- logic_test.go
+++ logic_test.go
@@ -103,3 +103,93 @@ func TestLogicHorizontalAlign(t *testing.T) {
 		})
 	}
 }
+
+func TestLogicVerticalAlign(t *testing.T) {
+	testFile := filepath.Join("testdata", "sample2.txt")
+	assert.NoError(t, ioutil.WriteFile(testFile, []byte("sushi"), os.ModePerm))
+	defer os.Remove(testFile)
+
+	tests := []struct {
+		desc    string
+		param   LogicVerticalAlignParam
+		wantErr bool
+	}{
+		{
+			desc: "正常系: AlignVerticalTop",
+			param: LogicVerticalAlignParam{
+				args: []string{
+					"README.adoc",
+				},
+				length:    100,
+				writeFile: false,
+				lineFeed:  "\n",
+				f:         AlignVerticalTop,
+			},
+			wantErr: false,
+		},
+		{
+			desc: "正常系: AlignVerticalCenter",
+			param: LogicVerticalAlignParam{
+				args: []string{
+					"README.adoc",
+				},
+				length:    100,
+				writeFile: false,
+				lineFeed:  "\n",
+				f:         AlignVerticalCenter,
+			},
+			wantErr: false,
+		},
+		{
+			desc: "正常系: AlignVerticalBottom",
+			param: LogicVerticalAlignParam{
+				args: []string{
+					"README.adoc",
+				},
+				length:    100,
+				writeFile: false,
+				lineFeed:  "\n",
+				f:         AlignVerticalBottom,
+			},
+			wantErr: false,
+		},
+		{
+			desc: "正常系: ファイル書き込みに成功する",
+			param: LogicVerticalAlignParam{
+				args: []string{
+					testFile,
+				},
+				length:    100,
+				writeFile: true,
+				lineFeed:  "\n",
+				f:         AlignVerticalCenter,
+			},
+			wantErr: false,
+		},
+		{
+			desc: "異常系: 存在しないファイル",
+			param: LogicVerticalAlignParam{
+				args: []string{
+					"foobar.txt",
+				},
+				length:    100,
+				writeFile: false,
+				lineFeed:  "\n",
+				f:         AlignVerticalCenter,
+			},
+			wantErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.desc, func(t *testing.T) {
+			assert := assert.New(t)
+			err := LogicVerticalAlign(tt.param)
+			if tt.wantErr {
+				assert.Error(err)
+				return
+			}
+			assert.NoError(err)
+		})
+	}
+}
