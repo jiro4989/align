b0e3d8cdea041fd9c46452273fc2fbac64f1bba9
jiro4989 <jiroron666@gmail.com>
2020-11-12 22:17:13 +0900

    chore: コマンドライン引数の設定方法を変更

--- cmd_right.go
+++ cmd_right.go
@@ -5,13 +5,21 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var (
+	rightPad       string
+	rightLength    int
+	rightWrite     bool
+	rightLineFeed  string
+	rightTermWidth bool
+)
+
 func init() {
 	RootCommand.AddCommand(rightCommand)
-	rightCommand.Flags().StringP("pad", "p", " ", "Padding string")
-	rightCommand.Flags().IntP("length", "n", -1, "Padding length")
-	rightCommand.Flags().BoolP("write", "w", false, "Overwrite file")
-	rightCommand.Flags().StringP("linefeed", "l", "\n", "Line feed")
-	rightCommand.Flags().BoolP("termwidth", "t", false, "Terminal width")
+	rightCommand.Flags().StringVarP(&rightPad, "pad", "p", " ", "Padding string")
+	rightCommand.Flags().IntVarP(&rightLength, "length", "n", -1, "Padding length")
+	rightCommand.Flags().BoolVarP(&rightWrite, "write", "w", false, "Overwrite file")
+	rightCommand.Flags().StringVarP(&rightLineFeed, "linefeed", "l", "\n", "Line feed")
+	rightCommand.Flags().BoolVarP(&rightTermWidth, "termwidth", "t", false, "Terminal width")
 }
 
 var rightCommand = &cobra.Command{
@@ -19,32 +27,13 @@ var rightCommand = &cobra.Command{
 	Aliases: []string{"r"},
 	Short:   "Align right command from file or stdin",
 	Run: func(cmd *cobra.Command, args []string) {
-		f := cmd.Flags()
-
-		p, err := f.GetString("pad")
-		if err != nil {
-			panic(err)
-		}
-
-		n, err := f.GetInt("length")
-		if err != nil {
-			panic(err)
-		}
-
-		writeFile, err := f.GetBool("write")
-		if err != nil {
-			panic(err)
-		}
-
-		lf, err := f.GetString("linefeed")
-		if err != nil {
-			panic(err)
-		}
-
-		useTermWidth, err := f.GetBool("termwidth")
-		if err != nil {
-			panic(err)
-		}
+		var (
+			p            = rightPad
+			n            = rightLength
+			writeFile    = rightWrite
+			lf           = rightLineFeed
+			useTermWidth = rightTermWidth
+		)
 
 		if useTermWidth {
 			if err := termbox.Init(); err != nil {
